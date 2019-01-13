6e234c7a81a3ffd2cbcf89689010cecac35e1387
Ashraf Cheraa <ccheraa@gmail.com>
2019-01-13 02:58:01 +0100

    added groups selection

--- pebble-extension/src/browser/dialogs/user-dialog.ts
+++ pebble-extension/src/browser/dialogs/user-dialog.ts
@@ -4,6 +4,9 @@ import { createError, PebbleError } from "../../classes/error";
 import { PebbleUser, PebbleUserData } from "../../classes/user";
 import { PebbleConnection } from "../../classes/connection";
 import { IDialogField, createField } from "../../classes/dialog-field";
+import { PebbleTabs } from "../../classes/tabs";
+import { createKeys, IKeysElement, addKey } from "../../classes/keys";
+import { Checkbox } from "../../classes/checkbox";
 
 @injectable()
 export class PebbleUserDialogProps extends DialogProps {
@@ -20,26 +23,56 @@ export interface PebbleUserDialogResult extends PebbleUserData {
 
 export class PebbleUserDialog extends AbstractDialog<PebbleUserDialogResult> {
 
+  protected readonly keys: IKeysElement = createKeys({});
+  protected readonly groups: { [k: string]: Checkbox } = {};
   protected readonly containerDiv: HTMLDivElement = document.createElement('div');
-  protected readonly username: IDialogField = createField('username', '');
-  protected readonly password: IDialogField = createField('password', '', 'password');
-  protected readonly passwordConfirm: IDialogField = createField('confirm password', '', 'password');
-  protected readonly passwordOld: IDialogField = createField('old password', '', 'password');
+  protected readonly username: IDialogField = createField('Username', '');
+  protected readonly password: IDialogField = createField('Password', '', 'password');
+  protected readonly passwordConfirm: IDialogField = createField('Confirm password', '', 'password');
+  protected readonly passwordOld: IDialogField = createField('Old password', '', 'password');
   protected readonly group: IDialogField;
+  protected readonly tabs: PebbleTabs = new PebbleTabs(['Credentials', 'Groups']);
 
   constructor(
     @inject(PebbleUserDialogProps) protected readonly props: PebbleUserDialogProps,
   ) {
     super(props);
     if (props.connection) {
-      this.group = createField('group', '', props.connection.groups.map(group => ({ label: group, value: group })));
-      this.containerDiv.appendChild(this.username.container);
-      // this.containerDiv.appendChild(this.passwordOld.container);
-      this.containerDiv.appendChild(this.password.container);
-      this.containerDiv.appendChild(this.passwordConfirm.container);
-      this.containerDiv.appendChild(this.group.container);
+      this.group = createField('Primary group', '', props.connection.groups.map(group => ({ label: group, value: group })));
+      this.group.input.addEventListener('change', e => Object.keys(this.groups).forEach(group => {
+        if (group === this.group.input.value) {
+          this.groups[group].disabled = true;
+          this.groups[group].checked = true;
+        } else {
+          this.groups[group].disabled = false;
+        }
+      }));
+      this.tabs.tabs.forEach(tab => tab.classList.add('vertical-form'));
       
-      this.containerDiv.className = 'dialog-container user-dialog-container vertical-form';
+      this.tabs.tabs[0].appendChild(this.username.container);
+      // this.tabs.tabs[0].appendChild(this.passwordOld.container);
+      this.tabs.tabs[0].appendChild(this.password.container);
+      this.tabs.tabs[0].appendChild(this.passwordConfirm.container);
+
+      this.keys.container.classList.add('no-label');
+      props.connection.groups.forEach(group => {
+        this.groups[group] = new Checkbox(group, group === this.group.input.value);
+        addKey(group, {
+          type: 'string',
+          value: '',
+          el: this.groups[group].container,
+        }, this.keys)
+      });
+      this.group.input.dispatchEvent(new Event('change'));
+      const label = document.createElement('div');
+      label.style.fontWeight = 'bold';
+      label.innerText = 'Groups';
+      this.tabs.tabs[1].appendChild(this.group.container);
+      this.tabs.tabs[1].appendChild(label);
+      this.tabs.tabs[1].appendChild(this.keys.container);
+      
+      this.containerDiv.appendChild(this.tabs.container);
+      this.containerDiv.className = 'dialog-container user-dialog-container';
       this.contentNode.appendChild(this.containerDiv);
 
       this.appendAcceptButton(props.acceptButton || 'Add');
@@ -53,7 +86,7 @@ export class PebbleUserDialog extends AbstractDialog<PebbleUserDialogResult> {
     return {
       enabled: true,
       expired: false,
-      groups: [],
+      groups: Object.keys(this.groups).filter(group => this.groups[group].checked),
       metadata: {},
       passwordOld: this.passwordOld.input.value,
       password: this.password.input.value,
@@ -64,7 +97,8 @@ export class PebbleUserDialog extends AbstractDialog<PebbleUserDialogResult> {
   }
 
   protected isValid(value: PebbleUserDialogResult, mode: DialogMode): DialogError {
-    return !!value.userName && !!value.password && value.password === value.passwordConfirm;
+    return true;
+    // return !!value.userName && !!value.password && value.password === value.passwordConfirm;
   }
 
   protected onAfterAttach(msg: Message): void {
@@ -74,6 +108,7 @@ export class PebbleUserDialog extends AbstractDialog<PebbleUserDialogResult> {
     this.addUpdateListener(this.password.input, 'input');
     this.addUpdateListener(this.passwordOld.input, 'input');
     this.addUpdateListener(this.passwordConfirm.input, 'input');
+    Object.keys(this.groups).forEach(group => this.addUpdateListener(this.groups[group].container, 'click'));
   }
 
   protected onActivateRequest(msg: Message): void {
