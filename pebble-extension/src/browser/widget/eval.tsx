eacba48e3464fc5f33a11a5145f207c57a963c1d
Ashraf Cheraa <ccheraa@gmail.com>
2019-01-15 05:40:00 +0100

    detect active editor changes

--- pebble-extension/src/browser/widget/eval.tsx
+++ pebble-extension/src/browser/widget/eval.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { ReactWidget, StatefulWidget } from "@theia/core/lib/browser";
 import { inject } from "inversify";
 import { PebbleCore } from '../core';
+import { EditorManager } from '@theia/editor/lib/browser';
+import { PebbleDocumentNode, PebbleNode } from '../../classes/node';
 
 export type PebbleEvalWidgetFactory = () => PebbleEvalWidget;
 export const PebbleEvalWidgetFactory = Symbol('PebbleEditorWidgetFactory');
 
 export class PebbleEvalWidget extends ReactWidget implements StatefulWidget {
+  protected documentNode: PebbleDocumentNode | undefined;
   constructor(
+    @inject(EditorManager) protected readonly manager: EditorManager,
     @inject(PebbleCore) protected readonly core: PebbleCore,
   ) {
     super();
@@ -18,12 +22,20 @@ export class PebbleEvalWidget extends ReactWidget implements StatefulWidget {
     this.title.closable = true;
     this.addClass('pebble-eval');
     this.update();
+
+    manager.onActiveEditorChanged(e => {
+      if (e) {
+        const node = this.core.getNode(e.editor.uri.path.toString());
+        if (PebbleNode.isDocument(node)) {
+          this.changeTo(node);
+        }
+      }
+    });
   }
 
-  protected render(): React.ReactNode {
-    return <pre>
-      XQuery evaluation will show up here.
-    </pre>;
+  changeTo(node: PebbleDocumentNode) {
+    this.documentNode = node;
+    this.update();
   }
 
   storeState(): object {
@@ -31,4 +43,14 @@ export class PebbleEvalWidget extends ReactWidget implements StatefulWidget {
   }
   
   restoreState(state: object) {}
+
+  protected render(): React.ReactNode {
+    if (this.documentNode) {
+      return <pre>Displaying: <strong>{this.documentNode.uri}</strong></pre>;
+    } else {
+      return <pre>
+        XQuery evaluation will show up here.
+      </pre>;
+    }
+  }
 }
