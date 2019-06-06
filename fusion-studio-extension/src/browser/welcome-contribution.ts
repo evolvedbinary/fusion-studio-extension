5df215de8311f55c256d4befc1b46d5becb4050e
Charafeddine Cheraa <ccheraa@gmail.com>
2019-06-06 14:13:17 +0100

    Added welcome view (#91)
    * renamed pebble to fusion-studio
    
    * updated theia packages to latest
    
    * added addons to travis config
    
    * fix: wrong extension version
    
    * fix: use local extension in development
    
    * open servers view on startup
    
    * cleanup
    
    * added welcome page
    
    * changed version

---
+++ fusion-studio-extension/src/browser/welcome-contribution.ts
@@ -0,0 +1,52 @@
+import { injectable, inject } from "inversify";
+import { AbstractViewContribution, CommonMenus } from "@theia/core/lib/browser";
+import { FSWelcomeWidget } from "./widget/welcome";
+import { FS_WELCOME_WIDGET_FACTORY_ID } from '../classes/welcome';
+import { FrontendApplicationStateService } from "@theia/core/lib/browser/frontend-application-state";
+import { MenuModelRegistry } from "@theia/core";
+
+@injectable()
+export class FSWelcomeContribution extends AbstractViewContribution<FSWelcomeWidget> {
+
+  constructor(
+    @inject(FrontendApplicationStateService) protected applicationStateService: FrontendApplicationStateService,
+  ) {
+    super({
+      widgetId: FS_WELCOME_WIDGET_FACTORY_ID,
+      widgetName: 'Welcome',
+      defaultWidgetOptions: {
+        mode: 'tab-after',
+      },
+      toggleCommandId: 'FusionEval:welcome',
+    });
+    this.applicationStateService.onStateChanged(e => {
+      if (e === 'initialized_layout') {
+        this.openView({ activate: true, reveal: true });
+        if (!localStorage.getItem('welcome.show')) {
+          this.openView({ toggle: true });
+        }
+      }
+    });
+  }
+
+  registerMenus(menus: MenuModelRegistry): void {
+    menus.registerMenuAction(CommonMenus.HELP, {
+      commandId: 'FusionEval:welcome',
+      label: 'Fusion Studio',
+      icon: 'fa fa-plug',
+      order: '0',
+    });
+  }
+
+  async initializeLayout(): Promise<void> {
+    await this.openView();
+  }
+
+  async onStart(): Promise<void> {
+    // TODO: load saved state
+  }
+
+  onStop(): void {
+    // TODO: save state
+  }
+}
---
+++ fusion-studio-extension/src/classes/welcome.ts
@@ -0,0 +1,1 @@
+export const FS_WELCOME_WIDGET_FACTORY_ID = 'fusion-welcome';
