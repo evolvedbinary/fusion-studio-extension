7ea9a61499dffad2954723e7d11efcb6706d7e70
Charafeddine Cheraa <ccheraa@gmail.com>
2021-03-18 00:47:58 +0100

    added directories upload tests

--- cypress/integration/uploads.js
+++ cypress/integration/uploads.js
@@ -1,5 +1,17 @@
 import { mkApiPathUrl, fsUrl } from '../support/config.js';
 import { dialogMainButton } from '../support/utils.js';
+
+const content = {
+  text: 'Test text file.',
+  xml: [
+    '<file>',
+    '<name>test.xml</name>',
+    '<modified>23 Mar 17 22:20</modified>',
+    '<owner>root</owner>',
+    '</file>',
+  ],
+}
+
 context('Fusion Studio', function () {
   before(function () {
     cy.visit(fsUrl)
@@ -9,7 +21,7 @@ context('Fusion Studio', function () {
     cy.addConnection();
     cy.waitForLoading();
     cy.getTreeNode(mkApiPathUrl('admin', '/db')).click();
-    // cy.addCollection(mkApiPathUrl('admin', '/db'), 'uploads');
+    cy.addCollection(mkApiPathUrl('admin', '/db'), 'uploads');
     cy.waitForLoading();
     cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads')).click();
   });
@@ -22,7 +34,7 @@ context('Fusion Studio', function () {
       cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/test.txt')).dblclick();
       cy.waitForLoading();
       cy.get('.p-Widget.p-TabBar li[title*=' + CSS.escape('/db/uploads/test.txt') + ']').click();
-      cy.get('[role=presentation].editor-scrollable').contains('Test text file.');
+      cy.get('[role=presentation].editor-scrollable').contains(content.text);
     });
     it('Multiple documents', function () {
       cy.dragFileTo(mkApiPathUrl('admin', '/db/uploads'), {
@@ -30,29 +42,43 @@ context('Fusion Studio', function () {
         'test.xml': 'test.xml',
       });
       cy.waitForLoading();
-      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/test2.txt')).should('be.visible').contains('test2.txt');
-      
-      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/test2.txt')).dblclick();
-      cy.waitForLoading();
-      cy.get('.p-Widget.p-TabBar li[title*=' + CSS.escape('/db/uploads/test2.txt') + ']').click();
-      cy.get('[role=presentation].editor-scrollable').contains('Test text file.');
-      
-      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/test.xml')).should('be.visible').contains('test.xml');
-
-      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/test.xml')).dblclick();
-      cy.waitForLoading();
-      cy.get('.p-Widget.p-TabBar li[title*=' + CSS.escape('/db/uploads/test.xml') + ']').click();
-      cy.get('.p-Widget.theia-editor.p-DockPanel-widget:not(.p-mod-hidden) [role=presentation].editor-scrollable').then(el => {
-        cy.get(el).contains('<file>');
-        cy.get(el).contains('<name>test.xml</name>');
-        cy.get(el).contains('<modified>23 Mar 17 22:20</modified>');
-        cy.get(el).contains('<owner>root</owner>');
-        cy.get(el).contains('</file>');
-      });
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/test2.txt'), content.text);
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/test.xml'), content.xml);
     });
-    // cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads')).rightclick();
-    // cy.getMenuCommand('fusion.delete').should('be.visible').click();
-    // cy.get(dialogMainButton).should('be.visible').click();
-    // cy.waitForLoading();
+    it('Directories', function () {
+      cy.dragFileTo(mkApiPathUrl('admin', '/db/uploads'), {
+        intro: 'test.txt',
+        'testDir': {
+          'description.md': 'test.txt',
+          'txts': {
+            'file1.txt': 'test.txt',
+            'file2.txt': 'test.txt',
+            'file3.txt': 'test.txt',
+          },
+          'xmls': {
+            'file1.xml': 'test.xml',
+            'file2.xml': 'test.xml',
+            'file3.xml': 'test.xml',
+          },
+        }
+      });
+      cy.waitForLoading();
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/intro'), content.text);
+      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/testDir')).click();
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/description.md'), content.text);
+      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/testDir/txts')).click();
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/txts/file1.txt'), content.text);
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/txts/file2.txt'), content.text);
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/txts/file3.txt'), content.text);
+      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads/testDir/xmls')).click();
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/xmls/file1.xml'), content.xml);
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/xmls/file2.xml'), content.xml);
+      cy.checkDocumentText(mkApiPathUrl('admin', '/db/uploads/testDir/xmls/file3.xml'), content.xml);
+      
+      cy.getTreeNode(mkApiPathUrl('admin', '/db/uploads')).rightclick();
+      cy.getMenuCommand('fusion.delete').should('be.visible').click();
+      cy.get(dialogMainButton).should('be.visible').click();
+      cy.waitForLoading();
+    });
   });
 });
