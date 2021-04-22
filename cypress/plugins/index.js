f407934531f99ba06e35a31d7dd1ee477a4db4a2
duncdrum <d.paterson@me.com>
2021-04-22 13:49:18 +0200

    fix(plugins): make circle green
    teporary fix see #379

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,6 +15,8 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
+  // TODO(DP): see #379 this needs to be made flexible
+
   config.env.API_PORT = process.env.API_PORT ? process.env.API_PORT : 4059;
   config.env.FS_PORT = process.env.FS_PORT ? process.env.FS_PORT : 3000;
   return config;
