361a3bf1fd55940740f08048068c14cb217d6501
duncdrum <d.paterson@me.com>
2021-05-19 23:20:23 +0200

    fix(connection_spec): isolate connection test
    add delete test
    add 2nd create route
    cleanup connections
    
    see #408, #399,

--- cypress/integration/01_connection_spec.js
+++ cypress/integration/01_connection_spec.js
@@ -1,33 +1,87 @@
 /// <reference types="Cypress" />
-context('Fusion Studio', function () {
-    describe('Connection Dialog', function () {
-        it('should accept user input', function () {
+context('Fusion Studio', () => {
+    describe('Connection Dialogue', () => {
+        after(() => {
+            // cleanup connections before each test
+            cy.visit('/')
+            cy.get('.fusion-item')
+                .rightclick()
+                .then(() => {
+                    cy.get('[data-command="fusion.disconnect"] > .p-Menu-itemLabel')
+                        .click()
+                    cy.get('.main').click()
+                })
+        })
+
+        it('should create a connection', () => {
             cy.visit('/')
             cy.get('#theia-top-panel .p-MenuBar-item').contains('File')
                 .click()
                 .then(() => {
                     cy.get('[data-command="fusion.connect"] > .p-Menu-itemLabel')
                         .contains('New Server.')
-                        .trigger('mousemove')
                         .click()
                     // set connection credentials
-            cy.get('div.name-field > input').clear().type('localhost')
-            cy.get('div.server-field > input').clear().type('http://localhost:8080')
-            cy.get('div.username-field > input').clear().type('admin')
-            cy.get('div.password-field > input').clear()
-            cy.get('.main').click()
+                    cy.get('div.name-field > input').clear().type('server1')
+                    cy.get('div.server-field > input').clear().type('http://localhost:8080')
+                    cy.get('div.username-field > input').clear().type('admin')
+                    cy.get('div.password-field > input').clear()
+                    cy.get('.main').click()
                 })
-            // set connection credentials
-            // cy.get('div.name-field > input').clear().type('localhost')
-            // cy.get('div.server-field > input').clear().type('http://localhost:8080')
-            // cy.get('div.username-field > input').clear().type('admin')
-            // const passwordField = cy.get('div.password-field > input').clear();
-            // // if (password) {
-            // //     passwordField.type(password);
-            // // }
-            // cy.get(dialogMainButton).click();
-            // cy.getTreeNode('admin' + '@' + 'http://localhost:8080').click();
-            // cy.waitForLoading();
-        });
+            // see it in action
+            cy.get('.ReactVirtualized__Grid')
+                .should('be.visible')
+                .should('contain', 'server1')
+            cy.get('.fusion-item')
+                .click()
+                .then(() => {
+                    cy.get('.ReactVirtualized__Grid__innerScrollContainer')
+                        .should('contain', 'db')
+                        .should('contain', 'RestXQ')
+                })
+        })
+
+        it('should fail gracefully', () => {
+            // Use a differet route
+            cy.get('#fusion-toolbar-button-add > .fa-fw')
+                .click()
+                .then(() => {
+                    // bad credentials
+                    cy.get('div.name-field > input').clear().type('server2')
+                    cy.get('div.server-field > input').clear().type('http://localhost:8080')
+                    cy.get('div.username-field > input').clear().type('badmin')
+                    cy.get('.main').click()
+                })
+
+            // see it in action
+            cy.get('.ReactVirtualized__Grid')
+                .should('be.visible')
+                .should('contain', 'server2')
+            cy.get(`[node-id=${CSS.escape('badmin@http://localhost:8080')}]`)
+                .click()
+                .then(() => {
+                    cy.get('.dialogContent')
+                        // TODO(DP): #408 this needs a meaningful error message
+                        .contains('error')
+                        cy.get('.dialogControl > .theia-button').click()
+                })
+        })
+
+        it('should remove bad connection', () => {
+            cy.get(`[node-id=${CSS.escape('badmin@http://localhost:8080')}]`)
+                .rightclick()
+                .then(() => {
+                    cy.get('[data-command="fusion.disconnect"] > .p-Menu-itemLabel')
+                        .click()
+                    cy.get('.dialogContent')
+                      .contains('you sure') 
+                    cy.get('.main').click()
+                })
+            cy.get('.ReactVirtualized__Grid')
+                .should('be.visible')
+                .should('not.contain', 'server2')    
+        })
     })
+
+
 })
