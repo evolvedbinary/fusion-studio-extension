5e07829a193fbbd5b2905a2d317cfe849a55e2c4
Charafeddine Cheraa <ccheraa@gmail.com>
2021-06-03 12:57:13 +0100

    better cleaner api tests Using intercept, fixtures, and aliases

--- cypress/integration/02_api-spec.js
+++ cypress/integration/02_api-spec.js
@@ -1,7 +1,5 @@
 /// <reference types="Cypress" />
 
-import { apiHost } from "../support/config";
-
 context('Talking to the api directly', () => {
   describe('API version', () => {
     before(function() {
@@ -9,24 +7,7 @@ context('Talking to the api directly', () => {
       cy.visit('/');
     });
     it('should fail to connect with older api', () => {
-      cy.window().then(function(win) {
-        cy.stub(win, 'fetch').callThrough().withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version').as('version api').resolves({
-          status: 200,
-          ok: true,
-          json: () => ({
-            version: '0.0.1',
-            "server": {
-              "product-name": "eXist",
-              "build": "20200123123609",
-              "version": "5.2.0",
-              "revision": "c58d04ec45de50e7738489dee072fcc863dc8b1b"
-            },
-            "exist-db": {
-              "compatible-version": null
-            }
-          }),
-        });
-      });
+      cy.intercept('GET', Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version', { fixture: 'bad_api' });
       cy.get('.fusion-item').click().then(() => {
         cy.get('.dialogTitle').should('contain.text', 'New Connection');
         cy.get('.dialogContent').should('be.visible')
@@ -36,27 +17,25 @@ context('Talking to the api directly', () => {
         cy.get('.dialogBlock').should('not.exist');
       });
     });
-    it.only('should connect with newer api', () => {
+    it('should connect with newer api', () => {
       cy.window().then(function(win) {
         const fetchSpy = cy.spy(win, 'fetch');
-        const versionSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version').as('version api');
-        const explorerSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/explorer?uri=/').as('explorer');
-        const userSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/user').as('user');
-        const groupSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/group').as('group');
-        const indexSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/index').as('index');
-        const restxqSpy = fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/restxq').as('restxq');
-        cy.get('.fusion-item').click().then(() => {
-          cy.get('.ReactVirtualized__Grid__innerScrollContainer')
-            .should('contain', 'db')
-            .should('contain', 'RestXQ').then(() => {
-              expect(versionSpy).to.be.called;
-              expect(explorerSpy).to.be.called;
-              expect(userSpy).to.be.called;
-              expect(groupSpy).to.be.called;
-              expect(indexSpy).to.be.called;
-              expect(restxqSpy).to.be.called;
-            });
-        });
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version').as('/version');
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/explorer?uri=/').as('/explorer');
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/user').as('/user');
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/group').as('/group');
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/index').as('/index');
+        fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/restxq').as('/restxq');
+        cy.get('.fusion-item').click();
+        cy.get('.fusion-view')
+          .should('contain', 'db')
+          .should('contain', 'RestXQ');
+        cy.get('@/version').should('be.called');
+        cy.get('@/explorer').should('be.called');
+        cy.get('@/user').should('be.called');
+        cy.get('@/group').should('be.called');
+        cy.get('@/index').should('be.called');
+        cy.get('@/restxq').should('be.called');
       });
     });
   });
