2d797b693d4b269442b89e4382af60bb1325036d
duncdrum <d.paterson@me.com>
2021-06-05 01:30:03 +0200

    fix(perm_spec): add permisson tests

--- cypress/integration/06_permission_spec.js
+++ cypress/integration/06_permission_spec.js
@@ -1,52 +1,119 @@
 /// <reference types="Cypress" />
-
 context('Permission Manager', () => {
     describe('working with tree view', () => {
-        before(() => {
-            cy.connect()
+      before(() => {
+        cy.connect()
+        cy.visit('/')
+        cy.get(`[node-id=${CSS.escape('admin@' + Cypress.env('API_HOST'))}]`)
+          .click()
+      })    
+  
+      // TODO(DP): note how the tree expansion for groups and users is memorized we need to clean that up
+  
+      describe('the security item', () => {
+        it('should have user entries', () => {
+          cy.get('[node-id$=security]')
+            .click()
+          cy.get('.ReactVirtualized__Grid')
+            .contains('Users')
+            .click()
+          cy.get('[node-id$=guest]')
+            .should('be.visible')
         })
-
-        describe('db context menu', () => {
-            it('display users and groups', function () {
-                // expand users and groups
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security]').should('be.visible').click();
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/user]').should('be.visible');
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/group]').should('be.visible');
-                // expand and check users
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/user]').click();
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-user.fs-icon + [node-id*=security\\/user\\/]').should('have.length.gt', 0);
-                // expand and check groups
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/group]').click();
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-users.fs-icon + [node-id*=security\\/group\\/]').should('have.length.gt', 0);
-            })
-            it('open user information', function () {
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/user]').should('be.visible');
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-user.fs-icon + [node-id*=security\\/user\\/]').should('have.length.gt', 0);
-                const firstUser = cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-user.fs-icon + [node-id*=security\\/user\\/]').first();
-                firstUser.then(user => {
-                    const userName = user.text();
-                    firstUser.rightclick();
-                    cy.get('.p-Widget.p-Menu .p-Menu-item[data-type=command][data-command=fusion\\.edit-user]').should('be.visible').click();
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogTitle').should('contain.text', 'Edit User: ' + userName);
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogContent .pb-body').should('be.visible').find('span + input.theia-input[type=text]').should('have.value', userName);
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogControl .theia-button.secondary').should('be.visible').click();
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell').should('not.exist');
-                });
-            })
-            it('open group information', function () {
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item [node-id$=security\\/group]').should('be.visible');
-                cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-users.fs-icon + [node-id*=security\\/group\\/]').should('have.length.gt', 0);
-                const firstGroup = cy.get('.p-Widget.theia-Tree .theia-TreeNode.fusion-item .fa-users.fs-icon + [node-id*=security\\/group\\/]').first();
-                firstGroup.then(group => {
-                    const groupName = group.text();
-                    firstGroup.rightclick();
-                    cy.get('.p-Widget.p-Menu .p-Menu-item[data-type=command][data-command=fusion\\.edit-group]').should('be.visible').click();
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogTitle').should('contain.text', 'Edit Group: ' + groupName);
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogContent .pb-body').should('be.visible').find('span + input.theia-input[type=text]').should('have.value', groupName);
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell .dialogBlock .dialogControl .theia-button.secondary').should('be.visible').click();
-                    cy.get('.p-Widget.dialogOverlay#theia-dialog-shell').should('not.exist');
-                });
-            })
+  
+        it('should let us create a new user', () => {
+          cy.get('[node-id$=security]')
+            .rightclick()
+            cy.get('[data-command="fusion.add-user"]')
+            .contains('Add')
+            .should('be.visible')
+          // TODO(DP): finish creating a test user
         })
+  
+        it('should display user properties card', () => {
+          cy.get('[node-id$=user\\/guest]')
+            .rightclick()
+          cy.get('.p-Menu > ul > .p-Menu-item')
+            .should('be.visible')
+            .should('have.length', 2)
+          cy.get('[data-command="fusion.edit-user"]')
+            .click()
+          cy.get('.dialogTitle')
+            .contains('guest')
+          cy.get('.pb-headers > a')
+            .should('have.length', 3)
+          cy.get('.dialogContent')
+            .contains('Username')
+          cy.get('.pb-headers > a:nth-child(2)')
+            .click()
+          cy.get('.dialogContent')
+            .contains('Primary group')
+          cy.get('.pb-headers > a:nth-child(3)')
+            .click()
+          cy.get('.active > .pb-tab > .keys > tr')
+            .should('have.length', 9)     
+          cy.get('.secondary')
+            .click()
+        })
+  
+        it.skip('should let us delete a user', () => {
+          cy.get('.ReactVirtualized__Grid')
+            .contains('Users')
+            .click()
+          cy.get('[node-id$=use\\/guest]')
+            .should('be.visible')
+        })
+  
+      })
+  
+      describe('the groups item', () => {
+        it('should have group entries', () => {
+          cy.get('.ReactVirtualized__Grid')
+            .contains('Groups')
+            .click()
+          cy.get('[node-id$=group\\/guest]')
+            .should('be.visible')
+        })
+  
+        it('should let us create a new group', () => {
+          cy.get('[node-id$=security]')
+            .rightclick()
+            cy.get('[data-command="fusion.add-group"]')
+            .contains('Add')
+            .should('be.visible')
+          // TODO(DP): finish creating a test group  
+        })
+  
+        it('should display group properties card', () => {
+          cy.get('[node-id$=group\\/guest]')
+            .rightclick()
+          cy.get('.p-Menu > ul > .p-Menu-item')
+            .should('be.visible')
+            .should('have.length', 2)
+          cy.get('[data-command="fusion.edit-group"]')
+            .click()
+          cy.get('.dialogTitle')
+            .contains('guest')
+          cy.get('.pb-headers > a')
+            .should('have.length', 2)
+          cy.get('.dialogContent')
+            .contains('Group name')
+          cy.get('.pb-headers > a:nth-child(2)')
+            .click()
+          cy.get('.active > .pb-tab > .keys > tr')
+            .should('have.length', 3)     
+          cy.get('.secondary')
+            .click()    
+        })
+  
+        it.skip('should let us delete a group', () => {
+          cy.get('.ReactVirtualized__Grid')
+            .contains('Users')
+            .click()
+          cy.get('[node-id$=use\\/guest]')
+            .should('be.visible')
+        })
+      })
+  
     })
-})
+  })
