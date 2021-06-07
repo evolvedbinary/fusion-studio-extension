9866319c9c4e41f53a86849b89d69399fd41aa7a
duncdrum <d.paterson@me.com>
2021-06-07 13:12:20 +0200

    fix(perm_spec): finish user and groups see #411 #430

--- cypress/integration/06_permission_spec.js
+++ cypress/integration/06_permission_spec.js
@@ -1,119 +1,144 @@
 /// <reference types="Cypress" />
 context('Permission Manager', () => {
-    describe('working with tree view', () => {
-      before(() => {
-        cy.connect()
-        cy.visit('/')
-        cy.get(`[node-id=${CSS.escape('admin@' + Cypress.env('API_HOST'))}]`)
+  describe('working with security item', () => {
+    before(() => {
+      cy.connect()
+      cy.visit('/')
+      cy.get(`[node-id=${CSS.escape('admin@' + Cypress.env('API_HOST'))}]`)
+        .click()
+    })
+
+    it('should have user entries', () => {
+      cy.get('[node-id$=security]')
+        .click()
+      cy.get('.ReactVirtualized__Grid')
+        .contains('Users')
+        .click()
+      cy.get('[node-id$=user\\/guest]')
+        .should('be.visible')
+    })
+
+    it('should have group entries', () => {
+      cy.get('.ReactVirtualized__Grid')
+        .contains('Groups')
+        .click()
+      cy.get('[node-id$=group\\/guest]')
+        .should('be.visible')
+    })
+
+    // TODO(DP): note how the tree expansion for groups and users is memorized we need to clean that up
+    describe('the user item', () => {
+      it('should let us create a new user', () => {
+        cy.get('[node-id$=security]')
+          .rightclick()
+        cy.get('[data-command="fusion.add-user"]')
+          .contains('Add')
+          .should('be.visible')
           .click()
-      })    
-  
-      // TODO(DP): note how the tree expansion for groups and users is memorized we need to clean that up
-  
-      describe('the security item', () => {
-        it('should have user entries', () => {
-          cy.get('[node-id$=security]')
-            .click()
-          cy.get('.ReactVirtualized__Grid')
-            .contains('Users')
-            .click()
-          cy.get('[node-id$=guest]')
-            .should('be.visible')
-        })
-  
-        it('should let us create a new user', () => {
-          cy.get('[node-id$=security]')
-            .rightclick()
-            cy.get('[data-command="fusion.add-user"]')
-            .contains('Add')
-            .should('be.visible')
-          // TODO(DP): finish creating a test user
-        })
-  
-        it('should display user properties card', () => {
-          cy.get('[node-id$=user\\/guest]')
-            .rightclick()
-          cy.get('.p-Menu > ul > .p-Menu-item')
-            .should('be.visible')
-            .should('have.length', 2)
-          cy.get('[data-command="fusion.edit-user"]')
-            .click()
-          cy.get('.dialogTitle')
-            .contains('guest')
-          cy.get('.pb-headers > a')
-            .should('have.length', 3)
-          cy.get('.dialogContent')
-            .contains('Username')
-          cy.get('.pb-headers > a:nth-child(2)')
-            .click()
-          cy.get('.dialogContent')
-            .contains('Primary group')
-          cy.get('.pb-headers > a:nth-child(3)')
-            .click()
-          cy.get('.active > .pb-tab > .keys > tr')
-            .should('have.length', 9)     
-          cy.get('.secondary')
-            .click()
-        })
-  
-        it.skip('should let us delete a user', () => {
-          cy.get('.ReactVirtualized__Grid')
-            .contains('Users')
-            .click()
-          cy.get('[node-id$=use\\/guest]')
-            .should('be.visible')
-        })
-  
+        cy.get(':nth-child(1) > .theia-input')
+          .type('cy-usr', { force: true })
+        // TODO(DP): better selector see #411
+        cy.get(':nth-child(2) > :nth-child(1) > .checkbox > .checkbox-box')
+          .click()
+        cy.get('.pb-headers > :nth-child(2)')
+          .click()
+        // s.a. #411
+        cy.get('.awesomplete > .theia-input')
+          .type('guest{enter}')
+        cy.get('.main')
+          .click()
+        cy.get('[node-id$=user\\/cy-usr]')
+          .should('be.visible')
       })
-  
-      describe('the groups item', () => {
-        it('should have group entries', () => {
-          cy.get('.ReactVirtualized__Grid')
-            .contains('Groups')
-            .click()
-          cy.get('[node-id$=group\\/guest]')
-            .should('be.visible')
-        })
-  
-        it('should let us create a new group', () => {
-          cy.get('[node-id$=security]')
-            .rightclick()
-            cy.get('[data-command="fusion.add-group"]')
-            .contains('Add')
-            .should('be.visible')
-          // TODO(DP): finish creating a test group  
-        })
-  
-        it('should display group properties card', () => {
-          cy.get('[node-id$=group\\/guest]')
-            .rightclick()
-          cy.get('.p-Menu > ul > .p-Menu-item')
-            .should('be.visible')
-            .should('have.length', 2)
-          cy.get('[data-command="fusion.edit-group"]')
-            .click()
-          cy.get('.dialogTitle')
-            .contains('guest')
-          cy.get('.pb-headers > a')
-            .should('have.length', 2)
-          cy.get('.dialogContent')
-            .contains('Group name')
-          cy.get('.pb-headers > a:nth-child(2)')
-            .click()
-          cy.get('.active > .pb-tab > .keys > tr')
-            .should('have.length', 3)     
-          cy.get('.secondary')
-            .click()    
-        })
-  
-        it.skip('should let us delete a group', () => {
-          cy.get('.ReactVirtualized__Grid')
-            .contains('Users')
-            .click()
-          cy.get('[node-id$=use\\/guest]')
-            .should('be.visible')
-        })
+
+      it('should display user properties card', () => {
+        cy.get('[node-id$=user\\/guest]')
+          .rightclick()
+        cy.get('.p-Menu > ul > .p-Menu-item')
+          .should('be.visible')
+          .should('have.length', 2)
+        cy.get('[data-command="fusion.edit-user"]')
+          .click()
+        cy.get('.dialogTitle')
+          .contains('guest')
+        cy.get('.pb-headers > a')
+          .should('have.length', 3)
+        cy.get('.dialogContent')
+          .contains('Username')
+        cy.get('.pb-headers > a:nth-child(2)')
+          .click()
+        cy.get('.dialogContent')
+          .contains('Primary group')
+        cy.get('.pb-headers > a:nth-child(3)')
+          .click()
+        cy.get('.active > .pb-tab > .keys > tr')
+          .should('have.length', 9)
+        cy.get('.secondary')
+          .click()
       })
-  
+
+      it('should let us delete a user', () => {
+        cy.get('[node-id$=user\\/cy-usr]')
+          .rightclick()
+        cy.get('[data-command="fusion.delete-user"]')
+          .click()
+        cy.get('.dialogContent')
+          .contains('Are you sure')
+        cy.get('.main')
+          .click()
+        cy.get('[node-id$=user\\/cy-usr]')
+          .should('not.exist')
+      })
+    })
+
+    describe('the groups item', () => {
+      it('should let us create a new group', () => {
+        cy.get('[node-id$=security]')
+          .rightclick()
+        cy.get('[data-command="fusion.add-group"]')
+          .contains('Add')
+          .should('be.visible')
+          .click()
+          .focused()
+          .type('cy-group{enter}', { force: true })
+        cy.get('[node-id$=group\\/cy-group]')
+          .should('be.visible')
+      })
+
+      it('should display group properties card', () => {
+        cy.get('[node-id$=group\\/guest]')
+          .rightclick()
+        cy.get('.p-Menu > ul > .p-Menu-item')
+          .should('be.visible')
+          .should('have.length', 2)
+        cy.get('[data-command="fusion.edit-group"]')
+          .click()
+        cy.get('.dialogTitle')
+          .contains('guest')
+        cy.get('.pb-headers > a')
+          .should('have.length', 2)
+        cy.get('.dialogContent')
+          .contains('Group name')
+        cy.get('.pb-headers > a:nth-child(2)')
+          .click()
+        cy.get('.active > .pb-tab > .keys > tr')
+          .should('have.length', 3)
+        cy.get('.secondary')
+          .click()
+      })
+
+      it('should let us delete a group', () => {
+        cy.get('[node-id$=group\\/cy-group]')
+          .rightclick()
+        cy.get('[data-command="fusion.delete-group"]')
+          .click()
+        cy.get('.dialogContent')
+          .contains('Are you sure')
+        cy.get('.main')
+          .click()
+        cy.get('[node-id$=user\\/cy-group]')
+          .should('not.exist')
+      })
     })
   })
+})
