f34dd5d01268701fae2e30fa5d6c30d983cdc043
duncdrum <d.paterson@me.com>
2021-09-13 13:59:44 +0200

    ignore(api_spec): lint

--- cypress/integration/01_api-spec.js
+++ cypress/integration/01_api-spec.js
@@ -5,7 +5,7 @@ context('Talking to the api directly', () => {
     before(function () {
       cy.connect()
       cy.visit('/')
-    });
+    })
     describe('With outdated API', () => {
       it('should fail to connect with older api', () => {
         cy.intercept('GET', Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version', { fixture: 'bad_api' })
@@ -16,19 +16,19 @@ context('Talking to the api directly', () => {
             .should('contain.text', 'You need to update your API to version "0.2.0" or higher')
           cy.get('.theia-button.main').should('be.visible').click()
           cy.get('.dialogBlock').should('not.exist')
-        });
-      });
+        })
+      })
     })
     describe('With current API', () => {
       it('should call all endpoints', () => {
         cy.window().then(function (win) {
-          const fetchSpy = cy.spy(win, 'fetch');
+          const fetchSpy = cy.spy(win, 'fetch')
           fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/version').as('/version')
           fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/explorer?uri=/').as('/explorer')
           fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/user').as('/user')
           fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/group').as('/group')
           fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/index').as('/index')
-          // See #508 
+          // See #508
           // fetchSpy.withArgs(Cypress.env('API_HOST') + '/exist/restxq/fusiondb/restxq').as('/restxq')
           cy.get('.fusion-item').click()
           cy.get('@/version').should('be.called')
@@ -36,19 +36,19 @@ context('Talking to the api directly', () => {
           cy.get('@/user').should('be.called')
           cy.get('@/group').should('be.called')
           cy.get('@/index').should('be.called')
-          // See #508 
+          // See #508
           // cy.get('@/restxq').should('be.called')
-        });
-      });
+        })
+      })
 
       it('should display tree roots', () => {
-        cy.get('.fusion-item').click()
+        cy.contains('localhost').click()
         cy.get('.fusion-view')
           .should('contain', 'db')
           .should('contain', 'Security')
           // see #508
           // .should('contain', 'RestXQ')          
-      });
+      })
     })
-  });
+  })
 })
